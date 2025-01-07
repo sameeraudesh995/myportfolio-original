8119c992e3838f81bc04ce9db66389e6941a7bd2
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2025-01-07 06:00:53 +0530

    service area created

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 // document.querySelector("section").classList.add("active");
+
+const resumeBtns=document.querySelectorAll('.resume-btn','.knight-rider-btn');
+
+resumeBtns.forEach((btn, idx)=>{
+    btn.addEventListener('click', ()=>{
+        const resumeDetails=document.querySelectorAll('.resume-details');
+        resumeBtns.forEach(btn =>{
+            btn.classList.remove('active');
+            btn.classList.remove('knight-rider-btn');
+        });
+       btn.classList.add('active');
+       btn.classList.add('knight-rider-btn');
+
+       resumeDetails.forEach(detail =>{
+        detail.classList.remove('active');
+    
+    });
+    resumeDetails[idx].classList.add('active');
+    });
+    
+});
